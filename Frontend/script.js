70479dd3e1891db7dbe99e4c934af00d001ff5f5
Gitesh Sorout <giteshsorout@gmail.com>
2026-07-20 23:21:48 +0530

    update some changes

--- Frontend/script.js
+++ Frontend/script.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:5000/api";
+const BASE_URL = "https://employee-payroll-system-t3mn.onrender.com";
 let cachedEmployees = [];
 
 // ================= TOAST FUNCTION =================
